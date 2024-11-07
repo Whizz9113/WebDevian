6dc28bf6636bb3101bb92467ac05a54e873fb327
Whizz9113 <141340840+Whizz9113@users.noreply.github.com>
2024-11-07 19:27:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg)$/i,
+      type: 'asset/resource'
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig 
